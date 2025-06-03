9d40e74946cd99bd35e9728a32e46655e60f56ca
Ajmal Alkhaledi <ajmalalkhaledi@gmail.com>
2025-06-03 17:10:34 +0300

    Version 2.0.0 03- June - 2025

--- app/Http/Controllers/ValidationUpdates.php
+++ app/Http/Controllers/ValidationUpdates.php
@@ -5,20 +5,30 @@ namespace App\Http\Controllers;
 use App\Events\UpdateReceived;
 use App\OrionTools\Utils;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ValidationUpdates extends Controller
 {
     public function handler(Request $request, string $api_key)
     {
-        event(new UpdateReceived(
-            updates: Utils::arrayToObject($request->all()),
-            data: [
-                'api_key' => $api_key,
-            ]
-        ));
-
-        return response()->json([
-            'status' => 'ok',
-        ]);
+        try {
+            event(new UpdateReceived(
+                updates: Utils::arrayToObject($request->all()),
+                data: [
+                    'api_key' => $api_key,
+                ]
+            ));
+        } catch (\Throwable $e) {
+            Log::error('Throwable in UpdateReceiverController@handler: ' . $e->getMessage(),[
+                'line' => $e->getLine(),
+                'file' => $e->getFile(),
+            ]);
+            report($e);
+        } finally {
+            return response()->json([
+                'status' => 'ok',
+            ]);
+        }
     }
 }
+
